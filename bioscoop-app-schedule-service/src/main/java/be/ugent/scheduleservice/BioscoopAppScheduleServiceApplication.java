4b801620957f822d0da5a8be42c5a45e7475c8ed
DESKTOP-31P65L6\ruben janssens <ruben.janssens@ugent.be>
2019-12-10 16:02:04 +0100

    schedule

---
+++ bioscoop-app-schedule-service/src/main/java/be/ugent/scheduleservice/BioscoopAppScheduleServiceApplication.java
@@ -0,0 +1,13 @@
+package be.ugent.scheduleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioscoopAppScheduleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BioscoopAppScheduleServiceApplication.class, args);
+    }
+
+}
