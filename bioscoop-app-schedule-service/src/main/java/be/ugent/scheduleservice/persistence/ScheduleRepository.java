88875ff21d022aa28ba3ff59d04c846ccb7a32c0
DESKTOP-31P65L6\ruben janssens <ruben.janssens@ugent.be>
2019-12-11 17:28:36 +0100

    schedule

--- bioscoop-app-schedule-service/src/main/java/be/ugent/scheduleservice/persistence/ScheduleRepository.java
+++ bioscoop-app-schedule-service/src/main/java/be/ugent/scheduleservice/persistence/ScheduleRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -17,14 +18,13 @@ public interface ScheduleRepository extends CrudRepository<Schedule,String>
 
     //public List<Schedule> getScheduleByBeginDate(LocalDateTime beginDate);
 
-
-    //@Query("SELECT s FROM schedule.schedule s WHERE cast(s.begin_date as date) = :day")
-    //@Query("SELECT s FROM Schedule s")
     @Query("SELECT s FROM Schedule s WHERE cast(s.beginDate as date) = cast(:day as date)")
-    //@Query("SELECT s FROM Schedule s")
     public List<Schedule> getScheduleByDay(String day);
 
 
 
+    //SELECT * FROM schedule.schedule WHERE (11 = zaal_nmr) AND NOT (('2018-11-02 05:00:00'<begin_date AND '2018-11-02 08:00:00'<begin_date) OR ('2018-11-02 05:00:00'>end_date AND '2018-11-02 08:00:00'>end_date))
+    @Query("SELECT s FROM Schedule s WHERE (:zaalNmr = s.zaalNmr) AND NOT ((cast(:beginTimestamp as timestamp)<s.beginDate AND cast(:endTimestamp as timestamp)<s.beginDate) OR (cast(:beginTimestamp as timestamp)>s.endDate AND cast(:endTimestamp as timestamp)>s.endDate))")
+    public List<Schedule> getMoviesAtTimeAndHall(String beginTimestamp,String endTimestamp,int zaalNmr);
 
 }
