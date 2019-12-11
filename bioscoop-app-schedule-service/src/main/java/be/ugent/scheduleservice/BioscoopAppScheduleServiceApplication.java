90402fdbbe02abdcbca87bdde1c716296d189769
DESKTOP-31P65L6\ruben janssens <ruben.janssens@ugent.be>
2019-12-11 14:31:35 +0100

    schedule

--- bioscoop-app-schedule-service/src/main/java/be/ugent/scheduleservice/BioscoopAppScheduleServiceApplication.java
+++ bioscoop-app-schedule-service/src/main/java/be/ugent/scheduleservice/BioscoopAppScheduleServiceApplication.java
@@ -1,13 +1,58 @@
 package be.ugent.scheduleservice;
 
+import be.ugent.scheduleservice.domain.Schedule;
+import be.ugent.scheduleservice.persistence.ScheduleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 public class BioscoopAppScheduleServiceApplication {
+    private static Logger logger= LoggerFactory.getLogger(BioscoopAppScheduleServiceApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(BioscoopAppScheduleServiceApplication.class, args);
     }
+
+    @Bean
+    public CommandLineRunner populateDatabase(ScheduleRepository scheduleRepository) {
+        return (args) ->{
+            logger.info("Insert");
+
+            scheduleRepository.deleteAll();
+            Schedule schedule =new Schedule();
+
+            LocalDateTime Nu = LocalDateTime.now();
+            //2010-02-02T01:05
+            schedule.setBeginDate(Nu);
+            schedule.setEndDate(Nu);
+            schedule.setZaalNmr(10);
+            schedule.setMediaId(1);
+            schedule.setMediaId(2);
+
+            scheduleRepository.save(schedule);
+
+
+
+
+            logger.info("Data in de DB");
+            for (Schedule s: scheduleRepository.findAll())
+            {
+                logger.info(s.getZaalNmr()+"");
+                logger.info(s.getBeginDate()+"");
+
+            }
+
+
+        };
+    }
+
 
 }
--- bioscoop-app-schedule-service/src/main/java/be/ugent/scheduleservice/persistence/ScheduleRepository.java
+++ bioscoop-app-schedule-service/src/main/java/be/ugent/scheduleservice/persistence/ScheduleRepository.java
@@ -5,13 +5,15 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface ScheduleRepository extends CrudRepository<Schedule,String>
 {
 
-    public List<Schedule> getScheduleByDay(LocalDate day);
+
+    public List<Schedule> getScheduleByBeginDate(LocalDateTime beginDate);
 
 
 
