dd7c4796c0484df0533c31e7ab27bf06cb72802b
Manu <schoenmakersmanu@gmail.com>
2019-12-21 11:34:14 +0100

    StatisticsService created

---
+++ bioscoop-app-statistics-service/src/main/java/be/ugent/statisticsservice/StatisticsserviceApplication.java
@@ -0,0 +1,13 @@
+package be.ugent.statisticsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticsserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatisticsserviceApplication.class, args);
+    }
+
+}
