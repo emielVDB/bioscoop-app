0400537c9b4b6c73a9e3bc5a04c392788c7543fd
DESKTOP-31P65L6\ruben janssens <ruben.janssens@ugent.be>
2019-12-29 21:39:10 +0100

    able to fail

--- bioscoop-app-schedule-service/src/main/java/be/ugent/scheduleservice/adapter/messaging/ScheduleEventHandler.java
+++ bioscoop-app-schedule-service/src/main/java/be/ugent/scheduleservice/adapter/messaging/ScheduleEventHandler.java
@@ -1,5 +1,7 @@
 package be.ugent.scheduleservice.adapter.messaging;
 
+import be.ugent.scheduleservice.domain.Schedule;
+import be.ugent.scheduleservice.domain.ScheduleWithAdTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.StreamListener;
@@ -10,15 +12,29 @@ public class ScheduleEventHandler {
     private static Logger logger = LoggerFactory.getLogger(ScheduleEventHandler.class);
 
     @StreamListener(Channels.ADDADSLOTS_REPLY)
-    public void addAdSlotsReply(int resp)
+    public void addAdSlotsReply(ScheduleWithAdTime resp)
     {
-        logger.info("hier addAdSlotsReply "+resp);
+        if(resp.getSeconds()==-1)
+        {
+            logger.info("failed to create a new ad slot");
+        }
+        else
+        {
+            logger.info("successfully created an ad slot");
+        }
     }
 
     @StreamListener(Channels.BOOKHALL_REPLY)
-    public void bookHallReply()
+    public void bookHallReply(Schedule s)
     {
-        logger.info("hier bookHallReply");
+        if(s.getZaalNmr()==-1)
+        {
+            logger.info("failed to book a hall");
+        }
+        else
+        {
+            logger.info("successfully booked a hall");
+        }
     }
 
 
