584051d762bf89bc4c55069ac6b7d0222000067a
Stijn Brysbaert <stijn.brysbaert@ugent.be>
2020-01-18 14:54:38 +0100

    frontend hall management

---
+++ frontend/js/hallmgmt.js
@@ -0,0 +1,50 @@
+
+var baseIp="http://192.168.99.100:3000";
+// var HallServiceAdr=baseIp+":3000/hall/";
+
+
+function GETHallByNumber() {
+    var hallNumber=$('#hallNumber').val();
+
+    var ourRequest=new XMLHttpRequest();
+    ourRequest.open('GET',baseIp + "/hall/" + hallNumber, true);
+    ourRequest.onload=function()
+    {
+        //alert(ourRequest.responseText);
+        $('#outputGetHallByNumber').val(ourRequest.responseText);
+
+    };
+    ourRequest.send();
+}
+
+function GETEventHallEventId() {
+    var eventId=$('#eventId').val();
+
+    var ourRequest=new XMLHttpRequest();
+    ourRequest.open('GET',baseIp + "/eventhall/" + eventId, true);
+    ourRequest.onload=function()
+    {
+        //alert(ourRequest.responseText);
+        $('#outputGetEventHallByEventId').val(ourRequest.responseText);
+
+    };
+    ourRequest.send();
+}
+
+// function POSTRemoveSchedule() {
+//     var eventId=$('#inputRemoveScheduleId').val();
+//
+//     var ourRequest=new XMLHttpRequest();
+//     ourRequest.open('POST',ScheduleServiceAdr+'remove', true);
+//     ourRequest.setRequestHeader("Content-type", "application/json");
+//
+//     ourRequest.onload=function()
+//     {
+//         //alert(ourRequest.responseText);
+//         $('#outputRemoveSchedule').val(ourRequest.responseText);
+//     };
+//
+//     var json=JSON.stringify({ "eventId": eventId });
+//
+//     ourRequest.send(json);
+// }
